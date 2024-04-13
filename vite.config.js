973e2f5a1bac95d051e40956a6bde90fab2ae244
anonymouse-persimmon <anonymous@persimmon.tokyo>
2024-04-13 14:31:32 +0900

    add _redirects file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(
+    ({ command, mode, isSsrBuild, isPreview }) => {
+      const config = {
+        plugins: [vue()],
+        build: {
+            rollupOptions: {
+                output: {
+                    entryFileNames: `_redirect`,
+                    chunkFileNames: `_redirect`
+                },
+            }
+        }
+      }
+      if (command === 'serve') {
+
+      } else if (command === 'build'){
+
+      }
+      return config
+    }
+)
