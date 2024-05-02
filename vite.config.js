72c27065e511983d0f2d4510ea563ea842c9582d
anonymouse-persimmon <anonymous@persimmon.tokyo>
2024-05-02 17:54:47 +0900

    use subdirectory

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig(
     ({ command, mode, isSsrBuild, isPreview }) => {
       const config = {
         plugins: [vue()],
+          build: {
+            outDir:"dist/modify-my-store/"
+          },
           base: "/modify-my-store/"
       }
       if (command === 'serve') {
